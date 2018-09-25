c4ca050b167f1233aa61799106e0ada3d42afe00
MaxDrljic <max_spree@hotmail.com>
2018-09-25 18:26:04 +0200

    Update view

--- notepad/urls.py
+++ notepad/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import create_view, list_view, delete_view
+from .views import create_view, list_view, delete_view, update_view
 
 app_name = 'notes'
 
 urlpatterns = [
     path('create/', create_view, name='create'),
     path('list/', list_view, name='list'),
-    path('<slug:id>/delete/', delete_view, name='delete')
+    path('<slug:id>/delete/', delete_view, name='delete'),
+    path('<slug:id>/update/', update_view, name='update')
 ]
