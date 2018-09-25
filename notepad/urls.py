b60143f546de1d47cb735cf3aa1a74ceaa83d115
MaxDrljic <max_spree@hotmail.com>
2018-09-25 18:11:01 +0200

    list and delete view

--- notepad/urls.py
+++ notepad/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import create_view
+from .views import create_view, list_view, delete_view
 
 app_name = 'notes'
 
 urlpatterns = [
-    path('create/', create_view, name='create')
+    path('create/', create_view, name='create'),
+    path('list/', list_view, name='list'),
+    path('<slug:id>/delete/', delete_view, name='delete')
 ]
