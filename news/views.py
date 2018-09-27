41e445f2f9b172cf7e00fede663e7735ff22cb8b
MaxDrljic <max_spree@hotmail.com>
2018-09-27 20:32:26 +0200

    Grabbing the data

--- news/views.py
+++ news/views.py
@@ -18,9 +18,15 @@ def scrape():
 
     soup = BeautifulSoup(content, "html.parser")
     # columns variable finds the div with a specific class on theonion.com
-    columns = soup.find_all(
-        'div', {'class': 'curation-module__zone'})
-    print(len(columns))
+    posts = soup.find_all(
+        'div', {'class': 'curation-module__item'})
+    print(len(posts))
+
+    for i in posts:
+        link = i.find_all('a', {'class': 'js_curation-click'})[1]
+        image_source = i.find('img', {'class': 'featured-image'})['data-src']
+        print(link.text)
+        print(image_source)
 
 
 scrape()
