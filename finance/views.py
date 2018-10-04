68509ebe22667d57a4902ca622b2d87f59a7e8f4
MaxDrljic <max_spree@hotmail.com>
2018-10-04 12:44:48 +0200

    Dash setup & _create_app() function added necessary styles and html

--- finance/views.py
+++ finance/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 # Create your views here.
+
+from django.views.decorators.csrf import csrf_exempt
 # rest_framework imports are referring to djangorestframework
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Company
+from .as_dash import dispatcher
 
 
 def company_article_list(request):
@@ -46,6 +49,9 @@ class ChartData(APIView):
 def dash(request, **kwargs):
     return HttpResponse(dispatcher(request))
 
+# This decorator marks a view as being exempt from the protection ensured by the middleware
 
+
+@csrf_exempt
 def dash_ajax(request):
     return HttpResponse(dispatcher(request), content_type='application/json')
