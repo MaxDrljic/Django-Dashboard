642bbeb1ab11e41f4b33a8a1dd7ed84d355cf405
MaxDrljic <max_spree@hotmail.com>
2018-10-04 19:47:40 +0200

    Dash component created

--- finance/as_dash.py
+++ finance/as_dash.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from pandas_datareader.data import DataReader
+import colorlover as cl
+import datetime as dt
 
 import dash
 import dash_core_components as dcc
@@ -8,6 +10,9 @@ import dash_html_components as html
 # Pandas library is used to read and use external data
 
 df_symbol = pd.read_csv('tickers.csv')
+
+# Colorscale from the colorlover docs
+colorscale = cl.scales['9']['qual']['Paired']
 
 # Used to get the request in the right format
 
@@ -29,44 +34,115 @@ def dispatcher(request):
             response = app.server.make_response(app.server.handle_exception(e))
         return response.get_data()
 
+# Create dash application ----------------->
+
 
 def _create_app():
     app = dash.Dash(csrf_protect=False)
     app.layout = html.Div([
-      html.Div([
-        html.H2('Quandl Finance Explorer',
-          style={
-            'display': 'inline',
-            'float': 'left',
-            'font-size': '2.65em',
-            'margin-left': '7px',
-            'font-weight': 'bolder',
-            'font-family': 'Product Sans',
-            'color': 'rgba(117, 117, 117, 0.95)',
-            'margin-top': '20px',
-            'margin-bottom': '0'
-          }),
-        html.A('Home', href='/home', style={
-          'color': 'red',
-          'display': 'inline',
-          'margin-left': '54%'
-          })
+        html.Div([
+            html.H2('Quandl Finance Explorer',
+                    style={
+                        'display': 'inline',
+                        'float': 'left',
+                        'font-size': '2.65em',
+                        'margin-left': '7px',
+                        'font-weight': 'bolder',
+                        'font-family': 'Product Sans',
+                        'color': 'rgba(117, 117, 117, 0.95)',
+                        'margin-top': '20px',
+                        'margin-bottom': '0'
+                    }),
+            html.A('Home', href='/home', style={
+                'color': 'red',
+                'display': 'inline',
+                'margin-left': '54%'
+            })
         ]),
-      dcc.Dropdown(
-        id='stock-ticker-input',
-        options=[{'label': s[0], 'value': s[1]}
-            for s in zip(df_symbol.Company, df_symbol.Symbol)],
-        value=['AAPL', 'TSLA'],
-        multi=True
-      ),
-      html.Div(id='graphs')
+        dcc.Dropdown(
+            id='stock-ticker-input',
+            options=[{'label': s[0], 'value': s[1]}
+                     for s in zip(df_symbol.Company, df_symbol.Symbol)],
+            value=['AAPL', 'TSLA'],
+            multi=True
+        ),
+        html.Div(id='graphs')
 
-      @app.callback(
-        dash.dependencies.Output('graphs', 'children'),
-        [dash.dependencies.Input('stock-ticker-input', 'value')]
-      )
+        @app.callback(
+            dash.dependencies.Output('graphs', 'children'),
+            [dash.dependencies.Input('stock-ticker-input', 'value')]
+        )
 
-      ])
     ])
 
-    return
+    # End dash application ----------------->
+
+
+def update_graph(tickers):
+    graphs = []
+    for i, ticker in enumerate(tickers):
+        try:
+            # df is dataframe
+            df = DataReader(ticker, 'quandl',
+                            df.datetime(2018, 1, 1),
+                            dt.datetime.now())
+        except:
+            graphs.append(html.H3(
+                'Data is not available for {}'.format(ticker),
+                style={'marginTop': 20, 'marginBottom': 20}
+            ))
+            continue
+
+        candlestick = {
+            'x': df.index,
+            'open': df['Open'],
+            'high': df['High'],
+            'low': df['Low'],
+            'close': df['Close'],
+            'type': 'candlestick',
+            'name': ticker,
+            'legendgroup': ticker,
+            'increasing': {'line': {'color': colorscale[0]}},
+            'decreasing': {'line': {'color': colorscale[1]}}
+        }
+
+        bb_bands = bbands(df.Close)
+        bollinger_traces = [{
+          'x': df.index,
+          'y': y,
+          'type': 'scatter',
+          'mode': 'lines',
+          'line': {'width': 1, 'color': colorscale[(i*2) % len(colorscale)]},
+          'hoverinfo': 'none',
+          'legendgroup': ticker,
+          'showlegend': True if i == 0 else False,
+          'name': '{} - bollinger bands'.format(ticker)
+        } for i, y in enumerate(bb_bands)]
+
+        graphs.append(dcc.Graph(
+          id=ticker,
+          figure={
+            'data': [candlestick] + bollinger_traces,
+            'layout': {
+              'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
+              'legend': {'x': 0}
+            }
+          }
+        ))
+
+        return graphs
+    return app        
+
+def bbands(price, window_size=10, num_of_std=5):
+  rolling_mean = price.rolling(window=window_size).mean()
+  rolling_std = price.rolling(window=window_size).std()
+  upper_band = rolling_mean + (rolling_std * num_of_std)        
+  lower_band = rolling_mean - (rolling_std * num_of_std)
+  return rolling_mean, upper_band, lower_band      
+
+
+# If this script is called, run the server
+# Condition specified for Windows OS
+if __name__ == "__main__":
+  app = _create_app()
+  app.run_server()
